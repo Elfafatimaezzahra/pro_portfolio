22820cb2db5bfd2d0566a5b58207b33683ab7214
Elfafatimaezzahra <felfahimi@gmail.com>
2023-05-02 13:35:04 +0100

    update

--- new_js.js
+++ new_js.js
@@ -1,5 +1,12 @@
-// const checkbox = document.getElementById('checkbox');
-
-// checkbox.addEventListener('change', ()=>{
-//   document.body.classList.toggle('dark');
-// })
+var menu = document.getElementsByClassName("menu");
+menu.style.maxHeight = "0px";
+function togglemenu(){
+    if(menu.style.maxHeight == "0px")
+    {
+        menu.style.maxHeight = "130px";
+    }
+    else
+    {
+        menu.style.maxHeight = "0px";
+    }
+}
